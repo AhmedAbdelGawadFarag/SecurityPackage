08451de29605e1d9e2c50f35d848c9e8415c1c9b
Ahmed Essam <AhmedEssam14B@gmail.com>
2022-04-17 15:38:53 +0200

    multiplicative Inverse done

--- securitylibrary/AES/AES.cs
+++ securitylibrary/AES/AES.cs
@@ -10,7 +10,6 @@ namespace SecurityLibrary.AES
     /// If the string starts with 0x.... then it's Hexadecimal not string
     /// </summary>
     /// 
-
     public class AES : CryptographicTechnique
     {
         string[] Rcon;
@@ -18,8 +17,8 @@ namespace SecurityLibrary.AES
         string[,] keySchedule = new string[4, 44];
         string[,] state = new string[4, 4];
         string[,] sBox;
-        string[,] mixColumns = new string[4, 4] { { "02", "03", "01", "01" }, { "01", "02", "03", "01" }, { "01", "01", "02", "03" }, { "03", "01", "01", "02" } };
-        string[,] invMixColumns = new string[4, 4] { { "0e", "0b", "0d", "09" }, { "09", "0e", "0b", "0d" }, { "0d", "09", "0e", "0b" }, { "0b", "0d", "09", "0e" } };
+        readonly string[,] mixColumns = new string[4, 4] { { "02", "03", "01", "01" }, { "01", "02", "03", "01" }, { "01", "01", "02", "03" }, { "03", "01", "01", "02" } };
+        readonly string[,] invMixColumns = new string[4, 4] { { "0e", "0b", "0d", "09" }, { "09", "0e", "0b", "0d" }, { "0d", "09", "0e", "0b" }, { "0b", "0d", "09", "0e" } };
         string[,] invSBox;
         void buildCipherTextMatrix(string cipherText)
         {
@@ -35,7 +34,6 @@ namespace SecurityLibrary.AES
         }
         void invShiftRowsRight()
         {
-            // shift state matrix rows 
             string[,] temp = new string[4, 4];
             for (int i = 0; i < 4; i++)
             {
@@ -92,8 +90,6 @@ namespace SecurityLibrary.AES
         }
         void invbuildKeySchedule(string key)
         {
-            // remove 0x, initial Round
-            // first 4 columns of key Schedule
             key = key.Substring(2);
             for (int j = 0; j < 4; j++)
             {
@@ -106,7 +102,6 @@ namespace SecurityLibrary.AES
             int round = 1;
             for (int col = 4; col < 44; col++)
             {
-                // first column of key schedule
                 if (col % 4 == 0)
                 {
                     string[,] temp = new string[4, 1];
@@ -141,51 +136,50 @@ namespace SecurityLibrary.AES
                     }
                 }
             }
-        } // done
+        } 
         string[,] ETable;
         void buildETable()
         {
             ETable = new string[16, 16]
-            {
-        { "01", "03", "05", "0F", "11", "33", "55", "FF", "1A", "2E", "72", "96", "A1", "F8", "13", "35"},
-        { "5F", "E1", "38", "48", "D8", "73", "95", "A4", "F7", "02", "06", "0A", "1E", "22", "66", "AA"},
-        { "E5", "34", "5C", "E4", "37", "59", "EB", "26", "6A", "BE", "D9", "70", "90", "AB", "E6", "31"},
-        { "53", "F5", "04", "0C", "14", "3C", "44", "CC", "4F", "D1", "68", "B8", "D3", "6E", "B2", "CD"},
-        { "4C", "D4", "67", "A9", "E0", "3B", "4D", "D7", "62", "A6", "F1", "08", "18", "28", "78", "88"},
-        { "83", "9E", "B9", "D0", "6B", "BD", "DC", "7F", "81", "98", "B3", "CE", "49", "DB", "76", "9A"},
-        { "B5", "C4", "57", "F9", "10", "30", "50", "F0", "0B", "1D", "27", "69", "BB", "D6", "61", "A3"},
-        { "FE", "19", "2B", "7D", "87", "92", "AD", "EC", "2F", "71", "93", "AE", "E9", "20", "60", "A0"},
-        { "FB", "16", "3A", "4E", "D2", "6D", "B7", "C2", "5D", "E7", "32", "56", "FA", "15", "3F", "41"},
-        { "C3", "5E", "E2", "3D", "47", "C9", "40", "C0", "5B", "ED", "2C", "74", "9C", "BF", "DA", "75"},
-        { "9F", "BA", "D5", "64", "AC", "EF", "2A", "7E", "82", "9D", "BC", "DF", "7A", "8E", "89", "80"},
-        { "9B", "B6", "C1", "58", "E8", "23", "65", "AF", "EA", "25", "6F", "B1", "C8", "43", "C5", "54"},
-        { "FC", "1F", "21", "63", "A5", "F4", "07", "09", "1B", "2D", "77", "99", "B0", "CB", "46", "CA" },
-        { "45", "CF", "4A", "DE", "79", "8B", "86", "91", "A8", "E3", "3E", "42", "C6", "51", "F3", "0E"},
-        { "12", "36", "5A", "EE", "29", "7B", "8D", "8C", "8F", "8A", "85", "94", "A7", "F2", "0D", "17"},
-        { "39", "4B", "DD", "7C", "84", "97", "A2", "FD", "1C", "24", "6C", "B4", "C7", "52", "F6", "01"}
+                {
+            { "01", "03", "05", "0F", "11", "33", "55", "FF", "1A", "2E", "72", "96", "A1", "F8", "13", "35"},
+            { "5F", "E1", "38", "48", "D8", "73", "95", "A4", "F7", "02", "06", "0A", "1E", "22", "66", "AA"},
+            { "E5", "34", "5C", "E4", "37", "59", "EB", "26", "6A", "BE", "D9", "70", "90", "AB", "E6", "31"},
+            { "53", "F5", "04", "0C", "14", "3C", "44", "CC", "4F", "D1", "68", "B8", "D3", "6E", "B2", "CD"},
+            { "4C", "D4", "67", "A9", "E0", "3B", "4D", "D7", "62", "A6", "F1", "08", "18", "28", "78", "88"},
+            { "83", "9E", "B9", "D0", "6B", "BD", "DC", "7F", "81", "98", "B3", "CE", "49", "DB", "76", "9A"},
+            { "B5", "C4", "57", "F9", "10", "30", "50", "F0", "0B", "1D", "27", "69", "BB", "D6", "61", "A3"},
+            { "FE", "19", "2B", "7D", "87", "92", "AD", "EC", "2F", "71", "93", "AE", "E9", "20", "60", "A0"},
+            { "FB", "16", "3A", "4E", "D2", "6D", "B7", "C2", "5D", "E7", "32", "56", "FA", "15", "3F", "41"},
+            { "C3", "5E", "E2", "3D", "47", "C9", "40", "C0", "5B", "ED", "2C", "74", "9C", "BF", "DA", "75"},
+            { "9F", "BA", "D5", "64", "AC", "EF", "2A", "7E", "82", "9D", "BC", "DF", "7A", "8E", "89", "80"},
+            { "9B", "B6", "C1", "58", "E8", "23", "65", "AF", "EA", "25", "6F", "B1", "C8", "43", "C5", "54"},
+            { "FC", "1F", "21", "63", "A5", "F4", "07", "09", "1B", "2D", "77", "99", "B0", "CB", "46", "CA" },
+            { "45", "CF", "4A", "DE", "79", "8B", "86", "91", "A8", "E3", "3E", "42", "C6", "51", "F3", "0E"},
+            { "12", "36", "5A", "EE", "29", "7B", "8D", "8C", "8F", "8A", "85", "94", "A7", "F2", "0D", "17"},
+            { "39", "4B", "DD", "7C", "84", "97", "A2", "FD", "1C", "24", "6C", "B4", "C7", "52", "F6", "01"}
             };
         }
         string[,] LTable;
         void buildLTable()
         {
-            LTable = new string[16, 16]
-            {
-        { "","00", "19", "01", "32", "02", "1A", "C6", "4B", "C7", "1B", "68", "33", "EE", "DF", "03"},
-        { "64", "04", "E0", "0E", "34", "8D", "81", "EF", "4C", "71", "08", "C8", "F8", "69", "1C", "C1"},
-        { "7D", "C2", "1D", "B5", "F9", "B9", "27", "6A", "4D", "E4", "A6", "72", "9A", "C9", "09", "78"},
-        { "65", "2F", "8A", "05", "21", "0F", "E1", "24", "12", "F0", "82", "45", "35", "93", "DA", "8E"},
-        { "96", "8F", "DB", "BD", "36", "D0", "CE", "94", "13", "5C", "D2", "F1", "40", "46", "83", "38"},
-        { "66", "DD", "FD", "30", "BF", "06", "8B", "62", "B3", "25", "E2", "98", "22", "88", "91", "10"},
-        { "7E", "6E", "48", "C3", "A3", "B6", "1E", "42", "3A", "6B", "28", "54", "FA", "85", "3D", "BA"},
-        { "2B", "79", "0A", "15", "9B", "9F", "5E", "CA", "4E", "D4", "AC", "E5", "F3", "73", "A7", "57"},
-        { "AF", "58", "A8", "50", "F4", "EA", "D6", "74", "4F", "AE", "E9", "D5", "E7", "E6", "AD", "E8"},
-        { "2C", "D7", "75", "7A", "EB", "16", "0B", "F5", "59", "CB", "5F", "B0", "9C", "A9", "51", "A0" },
-        { "7F", "0C", "F6", "6F", "17", "C4", "49", "EC", "D8", "43", "1F", "2D", "A4", "76", "7B", "B7"},
-        { "CC", "BB", "3E", "5A", "FB", "60", "B1", "86", "3B", "52", "A1", "6C", "AA", "55", "29", "9D"},
-        { "97", "B2", "87", "90", "61", "BE", "DC", "FC", "BC", "95", "CF", "CD", "37", "3F", "5B", "D1"},
-        { "53", "39", "84", "3C", "41", "A2", "6D", "47", "14", "2A", "9E", "5D", "56", "F2", "D3", "AB"},
-        { "44", "11", "92", "D9", "23", "20", "2E", "89", "B4", "7C", "B8", "26", "77", "99", "E3", "A5"},
-        { "67", "4A", "ED", "DE", "C5", "31", "FE", "18", "0D", "63", "8C", "80", "C0", "F7", "70", "07"}
+            LTable = new string[16, 16] {
+            { "","00", "19", "01", "32", "02", "1A", "C6", "4B", "C7", "1B", "68", "33", "EE", "DF", "03"},
+            { "64", "04", "E0", "0E", "34", "8D", "81", "EF", "4C", "71", "08", "C8", "F8", "69", "1C", "C1"},
+            { "7D", "C2", "1D", "B5", "F9", "B9", "27", "6A", "4D", "E4", "A6", "72", "9A", "C9", "09", "78"},
+            { "65", "2F", "8A", "05", "21", "0F", "E1", "24", "12", "F0", "82", "45", "35", "93", "DA", "8E"},
+            { "96", "8F", "DB", "BD", "36", "D0", "CE", "94", "13", "5C", "D2", "F1", "40", "46", "83", "38"},
+            { "66", "DD", "FD", "30", "BF", "06", "8B", "62", "B3", "25", "E2", "98", "22", "88", "91", "10"},
+            { "7E", "6E", "48", "C3", "A3", "B6", "1E", "42", "3A", "6B", "28", "54", "FA", "85", "3D", "BA"},
+            { "2B", "79", "0A", "15", "9B", "9F", "5E", "CA", "4E", "D4", "AC", "E5", "F3", "73", "A7", "57"},
+            { "AF", "58", "A8", "50", "F4", "EA", "D6", "74", "4F", "AE", "E9", "D5", "E7", "E6", "AD", "E8"},
+            { "2C", "D7", "75", "7A", "EB", "16", "0B", "F5", "59", "CB", "5F", "B0", "9C", "A9", "51", "A0" },
+            { "7F", "0C", "F6", "6F", "17", "C4", "49", "EC", "D8", "43", "1F", "2D", "A4", "76", "7B", "B7"},
+            { "CC", "BB", "3E", "5A", "FB", "60", "B1", "86", "3B", "52", "A1", "6C", "AA", "55", "29", "9D"},
+            { "97", "B2", "87", "90", "61", "BE", "DC", "FC", "BC", "95", "CF", "CD", "37", "3F", "5B", "D1"},
+            { "53", "39", "84", "3C", "41", "A2", "6D", "47", "14", "2A", "9E", "5D", "56", "F2", "D3", "AB"},
+            { "44", "11", "92", "D9", "23", "20", "2E", "89", "B4", "7C", "B8", "26", "77", "99", "E3", "A5"},
+            { "67", "4A", "ED", "DE", "C5", "31", "FE", "18", "0D", "63", "8C", "80", "C0", "F7", "70", "07"}
             };
         }
         string multiplyForInverseMixColumns(string A, string B)
@@ -206,7 +200,6 @@ namespace SecurityLibrary.AES
             {
                 sum = sum - Convert.ToInt32("FF", 16);
             }
-
             string ans = sum.ToString("X2");
             int row = Convert.ToInt32(ans.Substring(0, 1), 16);
             int col = Convert.ToInt32(ans.Substring(1, 1), 16);
@@ -234,14 +227,10 @@ namespace SecurityLibrary.AES
                     for (int j = 0; j < 4; j++)
                     {
                         string ans = multiplyForInverseMixColumns(tempColMixMatrix[j, 0], tempState[j, 0]);
-                        // convert temp and ans to binary
-                        //temp = Convert.ToString(Convert.ToInt32(temp, 16), 2).PadLeft(8, '0');
                         ans = Convert.ToString(Convert.ToInt32(ans, 16), 2).PadLeft(8, '0');
                         temp = XOR(temp, ans);
                     }
-                    //Console.WriteLine($"{i}, {col}");
                     state[i, col] = Convert.ToString(Convert.ToInt32(temp, 2), 16);
-                    //Console.WriteLine(state[i, col]);
                 }
             }
         }
@@ -266,7 +255,6 @@ namespace SecurityLibrary.AES
                 invShiftRowsRight();
                 invsBoxSubistitution(ref state, 4, 4);
             }
-
             addRoundKey(0);
             string output = "";
             for (int i = 0; i < 4; i++)
@@ -280,9 +268,7 @@ namespace SecurityLibrary.AES
                     output += state[j, i];
                 }
             }
-            Console.WriteLine("0x" + output);
             return "0x"+output;
-            //throw new NotImplementedException();
         }
 
         string XOR(string A, string B)
@@ -335,10 +321,6 @@ namespace SecurityLibrary.AES
                 }
 
                 string[,] tempColMixMatrix = new string[4, 1];
-                /* for(int i = 0; i < 4; i++)
-                 {
-                     tempColMixMatrix[i, 0] = mixColumns[col, i ];
-                 }*/
                 for (int i = 0; i < 4; i++)
                 {
                     for (int z = 0; z < 4; z++)
@@ -351,18 +333,13 @@ namespace SecurityLibrary.AES
                         if (tempColMixMatrix[j, 0] == "02") temp = XOR(temp, multiplyBy02(tempState[j, 0], false));
                         else if (tempColMixMatrix[j, 0] == "03") temp = XOR(temp, multiplyBy03(tempState[j, 0]));
                         else temp = XOR(temp, Convert.ToString(Convert.ToInt32(tempState[j, 0], 16), 2).PadLeft(8, '0'));
-
-                        //Console.WriteLine(temp);
                     }
-                    //Console.WriteLine($"{i}, {col}");
                     state[i, col] = Convert.ToString(Convert.ToInt32(temp, 2), 16);
-                    //Console.WriteLine(state[i, col]);
                 }
             }
         }
         void buildRconMatrix()
         {
-            // build Rcon matrix
             Rcon = new string[11];
             Rcon[0] = "01";
             Rcon[1] = "02";
@@ -375,8 +352,7 @@ namespace SecurityLibrary.AES
             Rcon[8] = "1b";
             Rcon[9] = "36";
             Rcon[10] = "6c";
-
-        } // done
+        } 
         void sBoxSubistitution(ref string[,] inputMatrix, int numberofrows, int numberofcols)
         {
             for (int i = 0; i < numberofrows; i++)
@@ -393,11 +369,9 @@ namespace SecurityLibrary.AES
                     inputMatrix[i, j] = sBox[row, col];
                 }
             }
-        } // done
+        }
         void buildKeySchedule(string key)
         {
-            // remove 0x, initial Round
-            // first 4 columns of key Schedule
             key = key.Substring(2);
             for (int j = 0; j < 4; j++)
             {
@@ -409,8 +383,7 @@ namespace SecurityLibrary.AES
             }
             int round = 1;
             for (int col = 4; col < 44; col++)
-            {
-                // first column of key schedule
+            { 
                 if (col % 4 == 0)
                 {
                     string[,] temp = new string[4, 1];
@@ -418,20 +391,15 @@ namespace SecurityLibrary.AES
                     {
                         temp[i, 0] = keySchedule[i, col - 1];
                     }
-                    // shift temp
                     string tempValue = temp[0, 0];
                     for (int i = 0; i < 3; i++)
                     {
                         temp[i, 0] = temp[i + 1, 0];
                     }
                     temp[3, 0] = tempValue;
-                    // subsistute with SBox
                     sBoxSubistitution(ref temp, 4, 1);
-                    // XOR with Rcon
-                    //Console.WriteLine(Rcon[round-1]);
                     temp[0, 0] = Convert.ToString(Convert.ToInt32(temp[0, 0], 16) ^ Convert.ToInt32(Rcon[round - 1], 16), 16);
                     round++;
-                    // add to keySchedule
                     for (int i = 0; i < 4; i++)
                     {
                         keySchedule[i, col] = Convert.ToString(Convert.ToInt32(keySchedule[i, col - 4], 16) ^ Convert.ToInt32(temp[i, 0], 16), 16);
@@ -445,7 +413,7 @@ namespace SecurityLibrary.AES
                     }
                 }
             }
-        } // done
+        } 
         void printState()
         {
             for (int i = 0; i < 4; i++)
@@ -458,10 +426,9 @@ namespace SecurityLibrary.AES
 
             }
             Console.WriteLine();
-        } // done
+        }
         void buildPlainTextMatrix(string plainText)
         {
-            // remove 0x
             plainText = plainText.Substring(2);
             for (int j = 0; j < 4; j++)
             {
@@ -471,7 +438,7 @@ namespace SecurityLibrary.AES
                     plainText = plainText.Substring(2);
                 }
             }
-        } // done
+        }
         void buildSBox()
         {
             sBox = new string[16, 16] {
@@ -492,10 +459,9 @@ namespace SecurityLibrary.AES
                 { "E1", "F8", "98", "11", "69", "D9", "8E", "94", "9B", "1E", "87", "E9", "CE", "55", "28", "DF" },
                 { "8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16" }
             };
-        } // done
+        }
         void shiftRowsLeft()
-        {
-            // shift state matrix rows 
+        { 
             string[,] temp = new string[4, 4];
             for (int i = 0; i < 4; i++)
             {
@@ -511,20 +477,16 @@ namespace SecurityLibrary.AES
                     state[i, j] = temp[i, (j + i) % 4];
                 }
             }
-        } // done
+        } 
         void addRoundKey(int round)
         {
-            // add round key
-
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
                     state[i, j] = Convert.ToString(Convert.ToInt32(state[i, j], 16) ^ Convert.ToInt32(keySchedule[i, j + round * 4], 16), 16);
-                    //Console.Write(j + round * 4);
                 }
             }
-            //printState();
         }
         public override string Encrypt(string plainText, string key)
         {
@@ -534,7 +496,6 @@ namespace SecurityLibrary.AES
             buildKeySchedule(key);
             buildPlainTextMatrix(plainText);
             addRoundKey(0);
-
             for (int round = 1; round <= 9; round++)
             {
                 sBoxSubistitution(ref state, 4, 4);
@@ -545,7 +506,6 @@ namespace SecurityLibrary.AES
             sBoxSubistitution(ref state, 4, 4);
             shiftRowsLeft();
             addRoundKey(10);
-            //printState();
             string cipherText = "";
             for (int i = 0; i < 4; i++)
             {
